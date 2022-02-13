296b346e75667cd00229460ad831fcdab67e6c5f
braveadmin <adriobkork@gmail.com>
2022-02-13 18:55:14 +0000

    script beta working. Backups remaining.

--- mc-install-script.sh
+++ mc-install-script.sh
@@ -28,17 +28,49 @@ if [[ ! $(lsblk /dev/sdb -no fstype) ]]; then 	#if lsblk doesn't return anything
 	echo "disk not partitioned";
         echo "starting the partitioning process..."	
 	mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-minecraft-disk
-	echo "mouting disk..."
-        mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft	
 else 
 	echo "disk is already partitioned";
 fi
 
+echo "mouting disk..."
+mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft	
+
 # Install and run the Minecraft server
 
+echo "installing dependencies"
 apt update
-apt-get install -y default-jre-headless 	#installing dependencies
 
+apt install -y software-properties-common screen
 
+apt-add-repository
 
+apt update
 
+# check if java is installed and install if needed
+
+if [ $(dpkg-query -W -f='${Status}' openjdk-17-jre-headless 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install -y openjdk-17-jre-headless;
+fi
+
+cd /home/minecraft
+
+echo "downloading minecraft server file..."
+
+sudo su
+
+curl https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar > minecraft_server.1.18.1.jar
+
+echo "starting server for first time..."
+
+java -Xms1G -Xmx7G -jar minecraft_server.1.18.1.jar nogui
+
+# sign EULA
+
+echo "signing EULA..."
+
+sed -i 's/false/true/g' eula.xt
+
+echo "starting Minecraft Server..."
+
+screen -S mcs java -Xms1G -Xmx7G -jar minecraft_server.1.18.1.jar nogui
